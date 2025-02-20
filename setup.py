f5b03706a86251ac61a85634c1c0430dd98bc122
Akshat Sharma <akshatsharma5877@gmail.com>
2025-02-20 11:05:25 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a basic emotion detection project using MLOps',
+    author='Akshat',
+    license='',
+)
